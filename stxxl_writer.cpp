3d6180dceed39fb442b664088fd41d5bceabfccc
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2016-06-01 15:11:51 +0200

    Added stxxl_writer

---
+++ stxxl_writer.cpp
@@ -0,0 +1,21 @@
+#include "stxxl_writer.h"
+
+//extern "C" {
+STXXLEdgeWriter *stxxl_writer_open_file(const char *filename, long long int elements_estimated) {
+    return new STXXLEdgeWriter(filename, elements_estimated);
+}
+
+void stxxl_writer_push(STXXLEdgeWriter *writer, const STXXLEdge *edge) {
+    writer->writer << *edge;
+    writer->edges_written++;
+}
+
+void stxxl_writer_close(STXXLEdgeWriter *writer) {
+    writer->writer.finish();
+    writer->vector.resize(writer->edges_written, true);
+    delete writer;
+}
+
+long long int stxxl_writer_edges(STXXLEdgeWriter *writer) {
+    return writer->edges_written;
+}
---
+++ stxxl_writer.h
@@ -0,0 +1,54 @@
+#ifndef _STXXL_WRITER_HEADER
+#define _STXXL_WRITER_HEADER
+
+// You may alter this type
+typedef struct STXXLEdge STXXLEdge;
+struct STXXLEdge {
+    long long int first;
+    long long int second;
+};
+
+#ifdef __cplusplus
+    #include <stxxl/io>
+    #include <stxxl/vector>
+
+    struct STXXLEdgeWriter {
+       typedef stxxl::VECTOR_GENERATOR<STXXLEdge>::result vector_type;
+       typedef vector_type::bufwriter_type bufwriter_type;
+
+       stxxl::linuxaio_file file;
+       vector_type vector;
+       bufwriter_type writer;
+       uint64_t edges_written;
+
+       STXXLEdgeWriter(const std::string & filename, uint64_t expected_num_elems = 0)
+             : file(filename, stxxl::file::DIRECT | stxxl::file::RDWR | stxxl::file::CREAT | stxxl::file::TRUNC)
+             , vector(&file)
+             , writer(vector.begin())
+             , edges_written(0)
+       {
+          if (expected_num_elems) {
+             vector.resize(expected_num_elems);
+          }
+       }
+    };
+#else
+    typedef struct STXXLEdgeWriter STXXLEdgeWriter;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    STXXLEdgeWriter* stxxl_writer_open_file(const char* filename, long long int elements_estimated);
+    void stxxl_writer_push(STXXLEdgeWriter* obj, const STXXLEdge* edge);
+    void stxxl_writer_close(STXXLEdgeWriter* obj);
+    long long int stxxl_writer_edges(STXXLEdgeWriter *writer);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _STXXL_WRITER_HEADER
